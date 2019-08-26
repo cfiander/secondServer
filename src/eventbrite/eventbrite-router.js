6d7c536b5e84a3bcf3b736f1c61af3a757d46af1
cfiander <cfiander90@gmail.com>
2019-08-26 00:48:43 -0700

    locations

--- src/eventbrite/eventbrite-router.js
+++ src/eventbrite/eventbrite-router.js
@@ -9,7 +9,6 @@ let userToken;
 eventbriteRouter
   .route(`/`)
   .get((req, res, next) => {
-    console.log(req.session)
     res.send({ url: `https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=I6MVEHHYVS3LD42Z46&redirect_uri=https://warm-bastion-62347.herokuapp.com/api/eventbrite/access` })
   })
 
@@ -18,14 +17,13 @@ eventbriteRouter
   .get((req, res, next) => {
     const code = req.query.code
     unirest.post('https://www.eventbrite.com/oauth/token')
-    .headers({"Content-Type": "application/x-www-form-urlencoded"})
-    .send({grant_type:"authorization_code", client_id:'I6MVEHHYVS3LD42Z46', client_secret:'V5MDVXPPD7JY5HNODIESMFVP32R63FXOCQS3ONC276SNQQTYBQ', code: `${code}`, redirect_uri: 'https://warm-bastion-62347.herokuapp.com/api/eventbrite/token' })
-    .end(function (response) {
-      userToken = response.body.access_token
-      req.session.token = true
-      console.log(req.session, 'two')
-      res.redirect('http://localhost:3000/eventbrite')
-    });
+      .headers({ "Content-Type": "application/x-www-form-urlencoded" })
+      .send({ grant_type: "authorization_code", client_id: 'I6MVEHHYVS3LD42Z46', client_secret: 'V5MDVXPPD7JY5HNODIESMFVP32R63FXOCQS3ONC276SNQQTYBQ', code: `${code}`, redirect_uri: 'https://warm-bastion-62347.herokuapp.com/api/eventbrite/token' })
+      .end(function (response) {
+        userToken = response.body.access_token
+        console.log(req.session, 'two')
+        res.redirect('http://localhost:3000/eventbritesearch')
+      });
   })
 
 eventbriteRouter
@@ -33,12 +31,26 @@ eventbriteRouter
   .get((req, res, next) => {
     const token = userToken
     unirest.get('https://www.eventbriteapi.com/v3/categories/')
-    .headers({'Authorization': `Bearer ${token}` })
-    .end(function (response) {
-      console.log(response.body);
-      res.send(response.body)
-    });
+      .headers({ 'Authorization': `Bearer ${token}` })
+      .end(function (response) {
+        console.log(response.body);
+        res.send(response.body)
+      });
   })
+
+eventbriteRouter
+  .route(`/locations`)
+  .post((req, res, next) => {
+    const token = userToken
+    const { location } = req.body.location
+    unirest.get(`https://www.eventbriteapi.com/v3/events/search?${location}.address=vancovuer&location.within=10km&expand=venue`)
+      .headers({ 'Authorization': `Bearer ${token}` })
+      .end(function (response) {
+        console.log(response.body);
+        res.send(response.body)
+      });
+  })
+
 
 
 
