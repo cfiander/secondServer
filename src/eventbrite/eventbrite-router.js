959fe1ed6aa29e261ffd789b6829485024c99f29
cfiander <cfiander90@gmail.com>
2019-08-25 18:37:31 -0700

    asdffdsa

--- src/eventbrite/eventbrite-router.js
+++ src/eventbrite/eventbrite-router.js
@@ -13,10 +13,25 @@ eventbriteRouter
 eventbriteRouter
   .route(`/access`)
   .get((req, res, next) => {
-    console.log(req.query)
-    res.send('a string')
+    const code = req.query.code
+    unirest.post('https://www.eventbrite.com/oauth/token')
+    .headers({"Content-Type": "application/x-www-form-urlencoded"})
+    .send({ body: `grant_type=authorization_code&client_id=I6MVEHHYVS3LD42Z46&client_secret=V5MDVXPPD7JY5HNODIESMFVP32R63FXOCQS3ONC276SNQQTYBQ&code=${code}&redirect_uri=https://warm-bastion-62347.herokuapp.com/api/eventbrite/token` })
+    .end(function (response) {
+      console.log(response.body);
+      res.send('success')
+    });
   })
 
+
+
+
+eventbriteRouter
+  .route(`/token`)
+  .get((req, res, next) => {
+    console.log(req)
+    res.send('a token string')
+  })
 
 
 module.exports = eventbriteRouter
