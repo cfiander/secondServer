c4c8e8e44b938f656771e5010498171378c87cac
cfiander <cfiander90@gmail.com>
2019-08-27 12:03:03 -0700

    fdas

--- src/eventbrite/eventbrite-router.js
+++ src/eventbrite/eventbrite-router.js
@@ -43,8 +43,8 @@ eventbriteRouter
   .route(`/events`)
   .post(jsonBodyParser, (req, res, next) => {
     const token = userToken
-    if (!req.body.search.query  || !req.body.search.location) {
-      throw error({message: 'Query and location are both required fields'})
+    if (!req.body.search.query || !req.body.search.location) {
+      throw error({ message: 'Query and location are both required fields' })
     }
     if (req.body.search.category === '' && req.body.search.subcategory === '') {
       const { query, location } = req.body.search
@@ -53,7 +53,7 @@ eventbriteRouter
         .end(function (response) {
           res.send(response.body)
         });
-    } 
+    }
     if (req.body.search.category) {
       const { query, location, category, subcategory } = req.body.search
       unirest.get(`https://www.eventbriteapi.com/v3/events/search/?q=${query}&location.address=${location}&location.within=40km&categories=${category}&subcategories=${subcategory}`)
@@ -61,18 +61,30 @@ eventbriteRouter
         .end(function (response) {
           res.send(response.body)
         });
-    } 
+    }
+  })
+
+eventbriteRouter
+  .route(`/organization`)
+  .post(jsonBodyParser, (req, res, next) => {
+    const { id } = req.body.venue
+    const token = userToken
+    unirest.get(`https://www.eventbriteapi.com/v3/venues/${id}/`)
+      .headers({ 'Authorization': `Bearer ${token}` })
+      .end(function (response) {
+        res.send(response.body)
+      });
   })
 
 eventbriteRouter
   .route(`/venue`)
   .post(jsonBodyParser, (req, res, next) => {
-    const { id } = req.body.venue
+    const { id } = req.body.organization
     const token = userToken
     console.log(token, 'token string')
     console.log(req.body, 'venue string one')
     console.log(id, 'venute two')
-    unirest.get(`https://www.eventbriteapi.com/v3/venues/${id}/`)
+    unirest.get(`https://www.eventbriteapi.com/v3/organizations/${id}/`)
       .headers({ 'Authorization': `Bearer ${token}` })
       .end(function (response) {
         res.send(response.body)
