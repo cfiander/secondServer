31cbaf9d960fce314a19251e71f148b5e2cbbbdd
cfiander <cfiander90@gmail.com>
2019-08-28 13:23:59 -0700

    fdsa

--- src/eventbrite/eventbrite-router.js
+++ src/eventbrite/eventbrite-router.js
@@ -74,7 +74,6 @@ eventbriteRouter
     }
     if (req.body.search.category === '' && req.body.search.subcategory === '') {
       const { query, location, page_number } = req.body.search
-      const {page_number} = req.body.page
       unirest.get(`https://www.eventbriteapi.com/v3/events/search/?q=${query}&location.address=${location}&location.within=40km&sort_by=date&page_number=${page_number}`)
         .headers({ 'Authorization': `Bearer ${token}` })
         .end(function (response) {
@@ -83,7 +82,6 @@ eventbriteRouter
     }
     if (req.body.search.category) {
       const { query, location, category, subcategory, page_number } = req.body.search
-      const {page_number} = req.body.page
       unirest.get(`https://www.eventbriteapi.com/v3/events/search/?q=${query}&location.address=${location}&location.within=40km&categories=${category}&subcategories=${subcategory}&sort_by=date&page_number=${page_number}`)
         .headers({ 'Authorization': `Bearer ${token}` })
         .end(function (response) {
